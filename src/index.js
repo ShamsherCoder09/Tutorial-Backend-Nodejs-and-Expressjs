6abb08db7cc211b774e03ffb49ffd30441c1a7c0
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-03-16 15:29:04 +0530

    project setup folders and files

---
+++ src/index.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const connectDB = async ()=>{
+//     try{
+//         const connectInstance = await mongoose.connect(`${process.env.MONGODB_URL}`)
+//         console.log('database is connected');
+//     } catch(error){
+//         console.log("connection database failed", error);
+//     }
+// }
+
+// export default connectDB;
+
+
+
+
+
+
+
+
+// PORT = 5000
+
+// CLOUDINARY_CLIENT_NAME=dx2gx8iyl
+// CLOUDINARY_CLIENT_API=514623876683756
+// CLOUDINARY_CLIENT_SECRET=5tu5dSnzMboVP7lhMfZTL5NsJQg
+
+// FRONTEND_URL=http://localhost:5173
+// MONGODB_URL=mongodb+srv://mohdshamsher365:mohd123456@cluster0.zesjyah.mongodb.net/MERN_JOB_SEEKING
+
+// JWT_SECRET_KEY=MOHDSHAMSHER12345
+// JWT_EXPIRE=7d
+
+// COOKIE_EXPIRE=5
