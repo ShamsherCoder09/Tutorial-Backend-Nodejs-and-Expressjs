b3c6db61d7d401983f128513628335b350586b86
Mohd Shamsher <mohdshamsher365@gmail.com>
2024-03-21 20:10:41 +0530

    refreshAccessToken created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -173,11 +173,53 @@ const logoutUser = asyncHandler(async(req, res)=>{
 });
 
 
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
 
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Unauthorized request");
+    }
+
+   try {
+     const decodedToken = jwt.verify(
+         incomingRefreshToken,
+         process.env.REFRESH_TOKEN_SECRET
+     );
+ 
+     const user = await findById(decodedToken?._id);
+     if(!user){
+         throw new ApiError(401, "Invalid refresh token");
+     }
+     if(incomingRefreshToken !== user?.refreshToken){
+         throw new ApiError(401, "Refresh token expired or used");
+     }
+ 
+     const {accessToken , newRefreshToken} = await generateAccessAndRefreshToken(user._id);
+     
+     const options = {
+         httpOnly:true,
+         secure:true
+     }
+ 
+     return res
+        .status(200)
+        .cookie("accessToken", accessToken,options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(
+         new ApiResponse(
+             200,
+         {accessToken, refreshToken: newRefreshToken},
+         "access token refreshed"
+         )
+     )
+   } catch (error) {
+     throw new ApiError(401, error?.message || "Invalid refresh token")
+   }
+})
 
 export { 
     registerUser,
     loginUser,
     logoutUser,
-    
+    refreshAccessToken
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser} from '../controllers/user.controller.js'
+import {registerUser, loginUser, logoutUser, refreshAccessToken} from '../controllers/user.controller.js'
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyToken from "../middlewares/auth.middleware.js";
 
@@ -20,6 +20,7 @@ router.route("/register").post(
     );
 
 router.route("/login").post(loginUser);
-router.route("/logout").post(verifyToken,logoutUser) 
+router.route("/logout").post(verifyToken,logoutUser);
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
